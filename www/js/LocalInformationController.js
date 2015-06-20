7978288352ca39e2bb7623d9c75451b5e8243f04
redspy <redspy@lycos.co.kr>
2015-06-20 17:52:46 +0900

    Ionic 에서 제공하는 기본 Pull to Refresh 일단 올림!
    Fixed #8
    근대 이거 쪼금 내리다 말면 글자가 남아있는 뻑이 존재함..

--- www/js/LocalInformationController.js
+++ www/js/LocalInformationController.js
@@ -1,5 +1,16 @@
 angular.module('starter.controllers')
 
-.controller('localInformationController', function ($scope) {
+.controller('localInformationController', function ($scope, $timeout) {
     $scope.name = "주변 상점 정보";
+
+    $scope.doRefresh = function () {
+        $timeout(function () {
+            //simulate async response
+            // Refresh
+            //$window.location.reload(true);
+            //Stop the ion-refresher from spinning
+            $scope.$broadcast('scroll.refreshComplete');
+
+        }, 1000);
+    };
 });
