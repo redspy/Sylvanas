69497d670c64647544c8082530992d81cc01634c
redspy <redspy@lycos.co.kr>
2015-06-21 18:30:24 +0900

    Floating 버튼 누르면 글쓰기 팝업 뜨도록 임시로 적용함!
    아직 더 다듬어야된다!!
    Fixed #10

--- www/js/LocalInformationController.js
+++ www/js/LocalInformationController.js
@@ -1,7 +1,35 @@
 angular.module('starter.controllers')
 
-.controller('localInformationController', function ($scope, $timeout) {
+.controller('localInformationController', function ($scope, $ionicModal, $timeout) {
     $scope.name = "주변 상점 정보";
+    
+
+    // Create the login modal that we will use later
+    $ionicModal.fromTemplateUrl('templates/AddAdStore.html', {
+        scope: $scope
+    }).then(function (modal) {
+        $scope.modal = modal;
+    });
+
+    // Triggered in the login modal to close it
+    $scope.closeWrite = function () {
+        $scope.modal.hide();
+    };
+
+    // Open the login modal
+    $scope.write = function () {
+        $scope.modal.show();
+    };
+
+    // Perform the login action when the user submits the login form
+    $scope.doWrite = function () {
+        // Simulate a login delay. Remove this and replace with your login
+        // code if using a login system
+        $timeout(function () {
+            $scope.closeWrite();
+        }, 1000);
+    };
+    
 
     $scope.doRefresh = function () {
         $timeout(function () {
