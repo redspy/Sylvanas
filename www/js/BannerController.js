4f2be79bb366578067c58097970cc6ab9cba8a22
redspy <redspy@lycos.co.kr>
2015-06-21 22:13:16 +0900

    배너광고에 더미 이미지 올리고 전화 연결 완료!
    Fixed #11
    아 근데 안드로이드는 클릭해도 반응이 없다.

--- www/js/BannerController.js
+++ www/js/BannerController.js
@@ -2,4 +2,15 @@ angular.module('starter.controllers')
 
 .controller('bannerController', function ($scope) {
     $scope.name = "배너광고";
+    
+    $scope.ads = [{ link : 'tel:123123123', banner : 'images-1.jpg', name : 'ionic' },
+                  { link : 'tel:123123123', banner : 'images.jpg', name : 'ionic' },
+                  { link : 'tel:123123123', banner : 'imgres-1.jpg', name : 'ionic' },
+                  { link : 'tel:123123123', banner : 'images-2.jpg', name : 'ionic' },
+                  { link : 'tel:123123123', banner : 'images-3.jpg', name : 'ionic' },
+                  { link : 'tel:123123123', banner : 'images-4.jpg', name : 'ionic' },
+                  { link : 'tel:123123123', banner : 'images-5.jpg', name : 'ionic' },
+                  { link : 'tel:123123123', banner : 'imgres.jpg', name : 'ionic' }
+                  ];
+        
 });
