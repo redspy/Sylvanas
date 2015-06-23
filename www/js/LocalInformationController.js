bfb81a7042552430851a77bca505391e83a1215f
redspy <redspy@lycos.co.kr>
2015-06-23 18:04:42 +0900

    카메라롤에서 사진 불러올 수 있도록 기능추가!

--- www/js/LocalInformationController.js
+++ www/js/LocalInformationController.js
@@ -1,6 +1,6 @@
 angular.module('starter.controllers')
 
-.controller('localInformationController', function ($scope, $ionicModal, $timeout) {
+.controller('localInformationController', function ($scope, $ionicModal, $timeout, $cordovaCamera, $cordovaFile) {
     $scope.name = "주변 상점 정보";
 
 
@@ -89,5 +89,81 @@ angular.module('starter.controllers')
         ];
         $scope.horizontalitems = $scope.horizontalitems.concat(tmp);
     };
+
+
+
+    $scope.images = [];
+
+    $scope.addImage = function() {
+        // 2
+        var options = {
+            destinationType : Camera.DestinationType.FILE_URI,
+            sourceType : Camera.PictureSourceType.PHOTOLIBRARY,// Camera.PictureSourceType.CAMERA, // Camera.PictureSourceType.PHOTOLIBRARY
+            allowEdit : false,
+            encodingType: Camera.EncodingType.JPEG,
+            popoverOptions: CameraPopoverOptions,
+        };
+
+        // 3
+        $cordovaCamera.getPicture(options).then(function(imageData) {
+
+            // 4
+            onImageSuccess(imageData);
+
+            function onImageSuccess(fileURI) {
+                createFileEntry(fileURI);
+            }
+
+            function createFileEntry(fileURI) {
+                window.resolveLocalFileSystemURL(fileURI, copyFile, fail);
+            }
+
+            // 5
+            function copyFile(fileEntry) {
+                var name = fileEntry.fullPath.substr(fileEntry.fullPath.lastIndexOf('/') + 1);
+                var newName = makeid() + name;
+
+                window.resolveLocalFileSystemURL(cordova.file.dataDirectory, function(fileSystem2) {
+                    fileEntry.copyTo(
+                        fileSystem2,
+                        newName,
+                        onCopySuccess,
+                        fail
+                    );
+                },
+                fail);
+            }
+
+            // 6
+            function onCopySuccess(entry) {
+                $scope.$apply(function () {
+                    $scope.images.push(entry.nativeURL);
+                });
+            }
+
+            function fail(error) {
+                console.log("fail: " + error.code);
+            }
+
+            function makeid() {
+                var text = "";
+                var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+                for (var i=0; i < 5; i++) {
+                    text += possible.charAt(Math.floor(Math.random() * possible.length));
+                }
+                return text;
+            }
+
+        }, function(err) {
+            console.log(err);
+        });
+    }
+
+    $scope.urlForImage = function(imageName) {
+        var name = imageName.substr(imageName.lastIndexOf('/') + 1);
+        var trueOrigin = cordova.file.dataDirectory + name;
+        return trueOrigin;
+    }
 
 });
