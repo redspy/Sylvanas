509116b04950c1597bface45ce830a116a667d62
redspy <redspy@lycos.co.kr>
2015-06-29 22:28:08 +0900

    Notification 예제 등록 완료!
    이제 퍼즐 맞추기만 하면됨!!

--- www/js/FavoritesController.js
+++ www/js/FavoritesController.js
@@ -2,4 +2,19 @@ angular.module('starter.controllers')
 
 .controller('favoritesController', function ($scope) {
     $scope.name = "내 찜 목록";
+
+    function alertDismissed() {
+        // do something
+    }
+
+    $scope.notification = function () {
+        navigator.notification.beep();
+        navigator.notification.vibrate(2000);
+        navigator.notification.alert(
+            'You are the winner!',  // message
+            alertDismissed,         // callback
+            'Game Over',            // title
+            'Done'                  // buttonName
+        );
+    }
 });
