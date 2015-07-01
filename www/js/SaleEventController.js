20cba80ec7ae48add8a8e757e2655bab3b764d27
redspy <redspy@lycos.co.kr>
2015-07-01 10:48:13 +0900

    Image Slider 구현해서 예제 올림! 아이오닉은 정말 없는게 없구만..

--- www/js/SaleEventController.js
+++ www/js/SaleEventController.js
@@ -2,4 +2,25 @@ angular.module('starter.controllers')
 
 .controller('saleEventController', function ($scope) {
     $scope.name = "할인 & 이벤트";
+
+    $scope.images = [
+        {
+            url :'images-1.jpg'
+        },
+        {
+            url :'images-2.jpg'
+        },
+        {
+            url :'images-3.jpg'
+        },
+        {
+            url :'images-4.jpg'
+        },
+        {
+            url :'images-5.jpg'
+        },
+        {
+            url :'images.jpg'
+        }
+    ]
 });
