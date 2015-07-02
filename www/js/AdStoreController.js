81bc93a48f7d879ece1ac38fdae8da004e16bdc2
redspy <redspy@lycos.co.kr>
2015-07-03 00:35:36 +0900

    주변상점정보 페이지 와꾸 잡기 완료!
    안드로이드 개발하기 싫다 진짜 ㅠ

--- www/js/AdStoreController.js
+++ www/js/AdStoreController.js
@@ -1,7 +1,299 @@
 angular.module('starter.controllers')
 
-.controller('adStoreController', function ($scope, $cordovaGeolocation) {
+.controller('adStoreController', function ($scope, $ionicModal, $timeout, $cordovaCamera, $cordovaFile, $cordovaGeolocation, $state) {
     $scope.name = "내가게 알리기";
+
+    $scope.items = [
+        {
+            id : '0001',
+            title : '탄산수를 집에서 직접 만들어 먹자!!',
+            nickname : '넌이미사고있다',
+            uploaddate : '2015. 7. 1',
+            body : '지금까지 사먹던 바로 그것! 탄산수!! 이제는 집에서 만들어 드실 수 있습니다. 한번 먹어봐~! 장님이 벌떡 일어나고 앉음뱅이가 눈을 번쩍! 이제는 사먹지 마세요',
+            images : [
+                {
+                    url :'example1.jpg'
+                },
+                {
+                    url :'example2.jpg'
+                },
+                {
+                    url :'example3.jpg'
+                }
+            ]
+        },
+        {
+            id : '0002',
+            title : '무료로 앱 만들어 드립니다.',
+            uploaddate : '2015. 6. 30',
+            nickname : '일단보고가',
+            body : '핸드폰 앱 만들기! 별거 아닙니다.. 그까이꺼 그냥 대~~충 만들다 보면 대박이 나고, 대박이 나면 잘살아 지고, 잘살아 지면 죽어.',
+            url : '',
+            images : [
+                {
+                    url :'images-1.jpg'
+                },
+                {
+                    url :'images-2.jpg'
+                },
+                {
+                    url :'images-3.jpg'
+                }
+            ]
+        },
+    {
+        id : '0001',
+        title : '탄산수를 집에서 직접 만들어 먹자!!',
+        nickname : '넌이미사고있다',
+        uploaddate : '2015. 7. 1',
+        body : '지금까지 사먹던 바로 그것! 탄산수!! 이제는 집에서 만들어 드실 수 있습니다. 한번 먹어봐~! 장님이 벌떡 일어나고 앉음뱅이가 눈을 번쩍! 이제는 사먹지 마세요',
+        images : [
+            {
+                url :'example1.jpg'
+            },
+            {
+                url :'example2.jpg'
+            },
+            {
+                url :'example3.jpg'
+            }
+        ]
+    },
+    {
+        id : '0002',
+        title : '무료로 앱 만들어 드립니다.',
+        uploaddate : '2015. 6. 30',
+        nickname : '일단보고가',
+        body : '핸드폰 앱 만들기! 별거 아닙니다.. 그까이꺼 그냥 대~~충 만들다 보면 대박이 나고, 대박이 나면 잘살아 지고, 잘살아 지면 죽어.',
+        url : '',
+        images : [
+            {
+                url :'images-1.jpg'
+            },
+            {
+                url :'images-2.jpg'
+            },
+            {
+                url :'images-3.jpg'
+            }
+        ]
+    },
+{
+    id : '0001',
+    title : '탄산수를 집에서 직접 만들어 먹자!!',
+    nickname : '넌이미사고있다',
+    uploaddate : '2015. 7. 1',
+    body : '지금까지 사먹던 바로 그것! 탄산수!! 이제는 집에서 만들어 드실 수 있습니다. 한번 먹어봐~! 장님이 벌떡 일어나고 앉음뱅이가 눈을 번쩍! 이제는 사먹지 마세요',
+    images : [
+        {
+            url :'example1.jpg'
+        },
+        {
+            url :'example2.jpg'
+        },
+        {
+            url :'example3.jpg'
+        }
+    ]
+},
+{
+    id : '0002',
+    title : '무료로 앱 만들어 드립니다.',
+    uploaddate : '2015. 6. 30',
+    nickname : '일단보고가',
+    body : '핸드폰 앱 만들기! 별거 아닙니다.. 그까이꺼 그냥 대~~충 만들다 보면 대박이 나고, 대박이 나면 잘살아 지고, 잘살아 지면 죽어.',
+    url : '',
+    images : [
+        {
+            url :'images-1.jpg'
+        },
+        {
+            url :'images-2.jpg'
+        },
+        {
+            url :'images-3.jpg'
+        }
+    ]
+},
+    ];
+//글쓰기///////////////////////////////////////////////////////////////////////
+    // 글쓰기에서 입력되는 Data 저장 : inputData
+    $scope.inputData = [];
+
+    // 글쓰기에서 입력되는 Data의 초기화
+    $scope.refreshInputdata = function (){
+        $scope.inputData = {
+            title : '',
+            nickName : window.localStorage['nickName'] || '',
+            body : ''
+        };
+    };
+
+    // Create the write modal that we will use later
+    $ionicModal.fromTemplateUrl('templates/AddAdStore.html', {
+        scope: $scope
+    }).then(function (modal) {
+        $scope.modal = modal;
+    });
+
+    // Triggered in the write modal to close it
+    $scope.closeWrite = function () {
+        $scope.modal.hide();
+    };
+
+    // 글쓰기 Data 초기화한뒤 Open the write modal
+    $scope.write = function () {
+        $scope.refreshInputdata();
+        $scope.modal.show();
+    };
+
+    // 글쓰기 입력 후 글 올리기 버튼 눌렀을때
+    $scope.doWrite = function () {
+        $scope.images = [];
+        window.localStorage['nickName'] = $scope.inputData.nickName;
+        $scope.inputData = [];
+        $timeout(function () {
+            $scope.closeWrite();
+        }, 1000);
+    };
+//글쓰기///////////////////////////////////////////////////////////////////////
+
+    // Pull to Refresh 로 당겼다가 놨을때 Callback
+    $scope.doRefresh = function () {
+        $timeout(function () {
+            //simulate async response
+            // Refresh
+            //$window.location.reload(true);
+            //Stop the ion-refresher from spinning
+            $scope.$broadcast('scroll.refreshComplete');
+
+        }, 1000);
+    };
+
+    ////////////////////////////////////////////////////////////////////////////////Image modal
+    $ionicModal.fromTemplateUrl('templates/image-modal.html', {
+        scope: $scope,
+        animation: 'slide-in-up'
+    }).then(function(modal) {
+        $scope.imagemodal = modal;
+    });
+
+    $scope.openModal = function() {
+        $scope.imagemodal.show();
+    };
+
+    $scope.closeModal = function() {
+        $scope.imagemodal.hide();
+    };
+
+    //Cleanup the modal when we're done with it!
+    $scope.$on('$destroy', function() {
+        $scope.imagemodal.remove();
+    });
+    // Execute action on hide modal
+    $scope.$on('modal.hide', function() {
+        // Execute action
+    });
+    // Execute action on remove modal
+    $scope.$on('modal.removed', function() {
+        // Execute action
+    });
+    $scope.$on('modal.shown', function() {
+        console.log('Modal is shown!');
+    });
+
+    var selectedImageIndex = 0;
+
+    $scope.showImage = function(index) {
+        $scope.imageSrc = $scope.urlForImage($scope.images[index]);//'http://ionicframework.com/img/ionic-logo-blog.png';
+        selectedImageIndex = index;
+        $scope.openModal();
+    }
+
+    $scope.deleteImage = function () {
+        $scope.images.splice(selectedImageIndex, 1);
+    }
+    ////////////////////////////////////////////////////////////////////////////////
+    $scope.images = [];
+
+    $scope.addImage = function() {
+        // 2
+        var options = {
+            destinationType : Camera.DestinationType.FILE_URI,
+            sourceType : Camera.PictureSourceType.PHOTOLIBRARY,// Camera.PictureSourceType.CAMERA, // Camera.PictureSourceType.PHOTOLIBRARY
+            allowEdit : false,
+            encodingType: Camera.EncodingType.JPEG,
+            popoverOptions: CameraPopoverOptions,
+        };
+
+        // 3
+        $cordovaCamera.getPicture(options).then(function(imageData) {
+
+            // 4
+            onImageSuccess(imageData);
+
+            function onImageSuccess(fileURI) {
+                createFileEntry(fileURI);
+            }
+
+            function createFileEntry(fileURI) {
+                window.resolveLocalFileSystemURL(fileURI, copyFile, fail);
+            }
+
+            // 5
+            function copyFile(fileEntry) {
+                var name = fileEntry.fullPath.substr(fileEntry.fullPath.lastIndexOf('/') + 1);
+                var newName = makeid() + name;
+
+                window.resolveLocalFileSystemURL(cordova.file.dataDirectory, function(fileSystem2) {
+                    fileEntry.copyTo(
+                        fileSystem2,
+                        newName,
+                        onCopySuccess,
+                        fail
+                    );
+                },
+                fail);
+            }
+
+            // 6
+            function onCopySuccess(entry) {
+                $scope.$apply(function () {
+                    $scope.images.push(entry.nativeURL);
+                });
+            }
+
+            function fail(error) {
+                console.log("fail: " + error.code);
+            }
+
+            function makeid() {
+                var text = "";
+                var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+                for (var i=0; i < 5; i++) {
+                    text += possible.charAt(Math.floor(Math.random() * possible.length));
+                }
+                return text;
+            }
+
+        }, function(err) {
+            console.log(err);
+        });
+    }
+
+    $scope.urlForImage = function(imageName) {
+        var name = imageName.substr(imageName.lastIndexOf('/') + 1);
+        var trueOrigin = cordova.file.dataDirectory + name;
+        return trueOrigin;
+    }
+
+    $scope.updateEditor = function() {
+        var element = document.getElementById("input_textarea");
+        element.style.height = element.scrollHeight + "px";
+    };
+
+/* 현재위치로 맵 띄우기 예제
 
     var posOptions = {timeout: 10000, enableHighAccuracy: false};
     $cordovaGeolocation
@@ -66,5 +358,5 @@ angular.module('starter.controllers')
         $scope.map = map;
     }
     google.maps.event.addDomListener(window, 'load', initialize);
-
+현재위치로 맵 띄우기 예제 */
 });
--- www/js/LocalInformationController.js
+++ www/js/LocalInformationController.js
@@ -1,49 +1,13 @@
 angular.module('starter.controllers')
 
-.controller('localInformationController', function ($scope, $ionicModal, $timeout, $cordovaCamera, $cordovaFile, $state) {
+.controller('localInformationController', function ($scope, $state) {
     $scope.name = "주변 상점 정보";
 
-    // 글쓰기에서 입력되는 Data 저장 : inputData
-    $scope.inputData = [];
-
-    // 글쓰기에서 입력되는 Data의 초기화
-    $scope.refreshInputdata = function (){
-        $scope.inputData = {
-            title : '',
-            nickName : window.localStorage['nickName'] || '',
-            body : ''
-        };
-    };
-
-    // Create the write modal that we will use later
-    $ionicModal.fromTemplateUrl('templates/AddAdStore.html', {
-        scope: $scope
-    }).then(function (modal) {
-        $scope.modal = modal;
-    });
-
-    // Triggered in the write modal to close it
-    $scope.closeWrite = function () {
-        $scope.modal.hide();
-    };
-
-    // 글쓰기 Data 초기화한뒤 Open the write modal
-    $scope.write = function () {
-        $scope.refreshInputdata();
-        $scope.modal.show();
-    };
-
-    // 글쓰기 입력 후 글 올리기 버튼 눌렀을때
-    $scope.doWrite = function () {
-        $scope.images = [];
-        window.localStorage['nickName'] = $scope.inputData.nickName;
-        $scope.inputData = [];
-        $timeout(function () {
-            $scope.closeWrite();
-        }, 1000);
-    };
-
-    // Pull to Refresh 로 당겼다가 놨을때 Callback
+    var heights = [225, 300, 375, 450];
+    var widths = [75, 150, 225, 300, 375, 450, 500];
+    var PictureType = ['abstract', 'animals', 'business', 'cats', 'city', 'food', 'nightlife', 'fashion', 'people', 'nature', 'sports', 'technics', 'transport'];
+    $scope.items = [];
+    //$scope.items = getData(10);
     $scope.doRefresh = function () {
         $timeout(function () {
             //simulate async response
@@ -54,59 +18,7 @@ angular.module('starter.controllers')
 
         }, 1000);
     };
-
-    ////////////////////////////////////////////////////////////////////////////////Image modal
-    $ionicModal.fromTemplateUrl('templates/image-modal.html', {
-        scope: $scope,
-        animation: 'slide-in-up'
-    }).then(function(modal) {
-        $scope.imagemodal = modal;
-    });
-
-    $scope.openModal = function() {
-        $scope.imagemodal.show();
-    };
-
-    $scope.closeModal = function() {
-        $scope.imagemodal.hide();
-    };
-
-    //Cleanup the modal when we're done with it!
-    $scope.$on('$destroy', function() {
-        $scope.imagemodal.remove();
-    });
-    // Execute action on hide modal
-    $scope.$on('modal.hide', function() {
-        // Execute action
-    });
-    // Execute action on remove modal
-    $scope.$on('modal.removed', function() {
-        // Execute action
-    });
-    $scope.$on('modal.shown', function() {
-        console.log('Modal is shown!');
-    });
-
-    var selectedImageIndex = 0;
-
-    $scope.showImage = function(index) {
-        $scope.imageSrc = $scope.urlForImage($scope.images[index]);//'http://ionicframework.com/img/ionic-logo-blog.png';
-        selectedImageIndex = index;
-        $scope.openModal();
-    }
-
-    $scope.deleteImage = function () {
-        $scope.images.splice(selectedImageIndex, 1);
-    }
-    ////////////////////////////////////////////////////////////////////////////////
-
-
-    var heights = [225, 300, 375, 450];
-    var widths = [75, 150, 225, 300, 375, 450, 500];
-    var PictureType = ['abstract', 'animals', 'business', 'cats', 'city', 'food', 'nightlife', 'fashion', 'people', 'nature', 'sports', 'technics', 'transport'];
-    $scope.items = [];
-    //$scope.items = getData(10);
-
+    
     function getRandomItem(arrObject) {
         var rndIndex = Math.floor((Math.random() * arrObject.length) + 1);
         return arrObject[rndIndex - 1];
@@ -141,99 +53,4 @@ angular.module('starter.controllers')
             $scope.loadMore();
         }
     });
-
-
-    $scope.horizontalitems = [];
-
-    for (var i = 0; i <= 5; i++) {
-        var tmp = [
-            {desc: 'The Ramones', image:'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSulfJcjBhxxW2NBBn9KbE3B4BSeh0R7mQ38wUi_zpJlQrMoDWh_qFcMelE_tjtAERUPTc'},
-            {desc: 'The Beatles', image:'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTGpH07f9zeucoOs_stZyIFtBncU-Z8TDYmJgoFnlnxYmXjJEaitmxZNDkNvYnCzwWTySM'},
-            {desc: 'Pink Floyd', image:'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcT-FbU5dD_Wz472srRIvoZAhyGTEytx9HWGusbhYgSc2h0N6AqqRrDwzApmyxZoIlyxDcU'},
-            {desc: 'The Rolling Stones', image:'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcT6uwPPBnHfAAUcSzxr3iq9ou1CZ4f_Zc2O76i5A4IyoymIVwjOMXwUFTGSrVGcdGT9vQY'},
-            {desc: 'The Jimi Hendrix Experience', image:'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcRA3jz0uhVypONAKWUve80Q6HASvuvZiohl4Sru5ZihkAsjWiaGjocfxd0aC3H7EeFk5-I'},
-            {desc: 'Van Halen', image:'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcRIslVN9cJJ6YuV0y7JihAyA63JDhXGhkCVxHIRE-IoaF-rpefjIXO5osA24QvN9iCptC8'}
-        ];
-        $scope.horizontalitems = $scope.horizontalitems.concat(tmp);
-    };
-
-    $scope.images = [];
-
-    $scope.addImage = function() {
-        // 2
-        var options = {
-            destinationType : Camera.DestinationType.FILE_URI,
-            sourceType : Camera.PictureSourceType.PHOTOLIBRARY,// Camera.PictureSourceType.CAMERA, // Camera.PictureSourceType.PHOTOLIBRARY
-            allowEdit : false,
-            encodingType: Camera.EncodingType.JPEG,
-            popoverOptions: CameraPopoverOptions,
-        };
-
-        // 3
-        $cordovaCamera.getPicture(options).then(function(imageData) {
-
-            // 4
-            onImageSuccess(imageData);
-
-            function onImageSuccess(fileURI) {
-                createFileEntry(fileURI);
-            }
-
-            function createFileEntry(fileURI) {
-                window.resolveLocalFileSystemURL(fileURI, copyFile, fail);
-            }
-
-            // 5
-            function copyFile(fileEntry) {
-                var name = fileEntry.fullPath.substr(fileEntry.fullPath.lastIndexOf('/') + 1);
-                var newName = makeid() + name;
-
-                window.resolveLocalFileSystemURL(cordova.file.dataDirectory, function(fileSystem2) {
-                    fileEntry.copyTo(
-                        fileSystem2,
-                        newName,
-                        onCopySuccess,
-                        fail
-                    );
-                },
-                fail);
-            }
-
-            // 6
-            function onCopySuccess(entry) {
-                $scope.$apply(function () {
-                    $scope.images.push(entry.nativeURL);
-                });
-            }
-
-            function fail(error) {
-                console.log("fail: " + error.code);
-            }
-
-            function makeid() {
-                var text = "";
-                var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-
-                for (var i=0; i < 5; i++) {
-                    text += possible.charAt(Math.floor(Math.random() * possible.length));
-                }
-                return text;
-            }
-
-        }, function(err) {
-            console.log(err);
-        });
-    }
-
-    $scope.urlForImage = function(imageName) {
-        var name = imageName.substr(imageName.lastIndexOf('/') + 1);
-        var trueOrigin = cordova.file.dataDirectory + name;
-        return trueOrigin;
-    }
-
-    $scope.updateEditor = function() {
-        var element = document.getElementById("input_textarea");
-        element.style.height = element.scrollHeight + "px";
-    };
-
 });
