8022911fd29f75ed0c7138f202f699e5f35d8f91
Spencer Roh <spencer.roh@gmail.com>
2015-08-10 17:45:44 +0900

    KT 서버 주소로 배너 뜨도록 수정함.

--- www/js/config.js
+++ www/js/config.js
@@ -1,6 +1,6 @@
 angular.module('starter.controllers')
 
-//.constant('SERVICE_ENDPOINT', 'http://222.122.143.163/undercity/api')
-//.constant('IMAGE_ENDPOINT', 'http://222.122.143.163/undercity/api/images/');
-.constant('SERVICE_ENDPOINT', 'http://localhost/undercity/api')
-    .constant('IMAGE_ENDPOINT', 'http://localhost/undercity/api/images/');
+.constant('SERVICE_ENDPOINT', 'http://222.122.143.163/undercity/api')
+    .constant('IMAGE_ENDPOINT', 'http://222.122.143.163/undercity/api/images/');
+//.constant('SERVICE_ENDPOINT', 'http://localhost/undercity/api')
+//    .constant('IMAGE_ENDPOINT', 'http://localhost/undercity/api/images/');
