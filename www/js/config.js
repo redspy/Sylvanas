07b56e3255a18a3f48722dcd1d804df56e4f40f9
Spencer Roh <spencer.roh@gmail.com>
2015-08-13 16:47:04 +0900

    반짝떨이 기능 구현

--- www/js/config.js
+++ www/js/config.js
@@ -1,6 +1,6 @@
 angular.module('starter.controllers')
 
-.constant('SERVICE_ENDPOINT', 'http://222.122.143.163/undercity/api')
+    .constant('SERVICE_ENDPOINT', 'http://222.122.143.163/undercity/api')
     .constant('IMAGE_ENDPOINT', 'http://222.122.143.163/undercity/api/images/');
-//.constant('SERVICE_ENDPOINT', 'http://localhost/undercity/api')
-//    .constant('IMAGE_ENDPOINT', 'http://localhost/undercity/api/images/');
+//    .constant('SERVICE_ENDPOINT', 'http://192.168.1.2/undercity/api')
+//    .constant('IMAGE_ENDPOINT', 'http://192.168.1.2/undercity/api/images/');
---
+++ www/js/factories/lightningDealService.js
@@ -0,0 +1,23 @@
+angular.module('starter.controllers')
+
+    .factory('lightningDealService', ['$resource', 'SERVICE_ENDPOINT', function ($resource, SERVICE_ENDPOINT) {
+        'use strict';
+        return $resource(SERVICE_ENDPOINT + '/deal/:id/:count', {}, {
+            create: {
+                method: 'POST'
+            },
+            readAll: {
+                method: 'GET',
+                isArray: true
+            },
+            read: {
+                method: 'GET'
+            },
+            modify: {
+                method: 'POST'
+            },
+            delete: {
+                method: 'DELETE'
+            }
+        });
+    }]);
