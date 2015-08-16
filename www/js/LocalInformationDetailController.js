504f6e65083d0987f2e219aa10d7aa952e3c9f94
redspy <redspy@lycos.co.kr>
2015-08-16 21:29:30 +0900

    아놔 이렇게 하면 안되는건가 ㅠ

--- www/js/LocalInformationDetailController.js
+++ www/js/LocalInformationDetailController.js
@@ -1,35 +1,63 @@
 angular.module('starter.controllers')
 
-    .controller('localinformationdetailcontroller', ['$scope', '$stateParams', function ($scope, $stateParams) {
-        $scope.name = "localinformationdetailcontroller";
+    .controller('localinformationdetailcontroller', ['$scope', '$stateParams', 'storeService', '$q', 'IMAGE_ENDPOINT', function ($scope, $stateParams, storeService, $q, IMAGE_ENDPOINT) {
+        $scope.name = "";
         $scope.id = $stateParams.unitid;
+        $scope.item = [];
+        $scope.images = [];
+        /*
+         * 상점 정보를 from에 지정된 id 상점을 기준으로 count만큼의 상점정보를 리턴한다.
+         * 만일 count 만큼의 데이터가 없으면 있는 만큼만 리턴함.
+         * from: -1일 경우 제일 마지막에 입력된 상점을 기준으로 출력함.
+         * count: -1일 경우 from을 기준으로 모든 상점을 리턴한다.
+         * data Sample
+         * {
+         "Id": 15,
+         "Name": "상호명",
+         "Address": "주소",
+         "Contact": "031-254-7852",
+         "Product": "품목",
+         "Description": "소개",
+         "GPS": "37.548, 127.5",
+         "CreateDate": "2015-08-11T06:39:04Z",
+         "Images": [35, 36]
+         }
+         * */
+        function getStoreInformation(from, count) {
+            var deferred = $q.defer();
+
+            storeService.readAll(
+                {id:from, count:count},
+                function (value, responseHeaders) {
+                    deferred.resolve(value);
+                }, function (httpResponse) {
+                    deferred.reject(httpResponse)
+                });
+
+            return deferred.promise;
+        }
+
+        getStoreInformation($scope.id, 1).then(function (data) {
+            console.log(data);
+            $scope.images = [];
+            $scope.item = data[0];
+
+            $scope.item.lat = $scope.item.GPS.split(',')[0];
+            $scope.item.long = $scope.item.GPS.split(',')[1];
+            for (var i = 0; i < $scope.item.Images.length; i++) {
+                $scope.images.push($scope.getImageURL($scope.item.Images[i]));
+                //$scope.images[i].url = $scope.getImageURL($scope.item.Images[i]);
+            }
+            $scope.name = $scope.item.Name;
+            $scope.initialize();
+        });
+
+        $scope.getImageURL = function (imageID) {
+            return IMAGE_ENDPOINT + imageID;
+        };
 
         $scope.doSomething = function () {
             $scope.reply = "";
-        };
-
-        $scope.images = [
-            {
-                url: 'example1.jpg'
-            },
-            {
-                url: 'example2.jpg'
-            },
-            {
-                url: 'example3.jpg'
-            }
-        ];
-
-        $scope.store = {
-            marketname: "강릉 중앙시장",
-            name: "홍콩반점",
-            favoritcount: "3",
-            category: "짜장면",
-            phone: "010-6521-6240",
-            address: "강원도 강릉시 짜장면",
-            bodytext: "이 짜장면 집으로 말씀드릴것 같으면 소림사에서 30년간 연마한 무술과 내공으로 최고의 면빨을 자랑하기 블라블라.",
-            lat: "37.5069106",
-            long: "127.0658082"
         };
 
         $scope.favoriteClick = function () {
@@ -37,11 +65,11 @@ angular.module('starter.controllers')
         };
 
         $scope.callClick = function () {
-            window.open('tel:' + $scope.store.phone);
+            window.open('tel:' + $scope.item.Contact);
         };
 
         $scope.initialize = function () {
-            var myLatlng = new google.maps.LatLng($scope.store.lat, $scope.store.long);
+            var myLatlng = new google.maps.LatLng($scope.item.lat, $scope.item.long);
 
             var mapOptions = {
                 center: myLatlng,
@@ -69,6 +97,6 @@ angular.module('starter.controllers')
             });
             $scope.map = map;
         };
-        google.maps.event.addDomListener(window, 'load', $scope.initialize);
+        // google.maps.event.addDomListener(window, 'load', $scope.initialize);
 
     }]);
