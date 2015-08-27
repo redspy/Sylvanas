b1c380755ce7d28f2f89957f637b009f4c615d4a
redspy <redspy@lycos.co.kr>
2015-08-27 20:36:07 +0900

    내찜목록 일단 와꾸만 커밋해둠

--- www/js/FavoritesController.js
+++ www/js/FavoritesController.js
@@ -3,18 +3,28 @@ angular.module('starter.controllers')
     .controller('favoritesController', ['$scope', function ($scope) {
         $scope.name = "내 찜 목록";
 
-        function alertDismissed() {
-            // do something
-        }
+        $scope.items = [
+            {
+                Id : "1",
+                listType: "할인&이벤트",
+                Title: "이것은 제목이지롱",
+                body : "내용은 이러이러 합니다만,,",
+                image: "/img/images.jpg"
+            }
+        ];
 
-        $scope.notification = function () {
-            navigator.notification.beep();
-            navigator.notification.vibrate(2000);
-            navigator.notification.alert(
-                'You are the winner!',  // message
-                alertDismissed,         // callback
-                'Game Over',            // title
-                'Done'                  // buttonName
-            );
-        };
+        //function alertDismissed() {
+        //    // do something
+        //}
+        //
+        //$scope.notification = function () {
+        //    navigator.notification.beep();
+        //    navigator.notification.vibrate(2000);
+        //    navigator.notification.alert(
+        //        'You are the winner!',  // message
+        //        alertDismissed,         // callback
+        //        'Game Over',            // title
+        //        'Done'                  // buttonName
+        //    );
+        //};
     }]);
