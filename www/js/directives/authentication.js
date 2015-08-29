f738c2aed79ed86335c1109b21701c8798e60baa
Spencer Roh <spencer.roh@gmail.com>
2015-08-29 15:10:22 +0900

    로그인 시, RSA로 암호화하여 처리하도록 함.

--- www/js/directives/authentication.js
+++ www/js/directives/authentication.js
@@ -1,22 +1,37 @@
-angular.module('starter.controllers').directive('authentication', ['authenticationService', 'authService', '$ionicPlatform', '$http', function (authenticationService, authService, $ionicPlatform, $http) {
+angular.module('starter.controllers').directive('authentication', ['authenticationService', 'authService', '$ionicPlatform', '$http', 'certService', function (authenticationService, authService, $ionicPlatform, $http, certService) {
     var failed = 0;
     return {
         restrict: 'C',
-        link: function (scope, element, attrs) {
+        link: function (scope) {
             scope.$on('event:auth-loginRequired', function () {
                 console.log('login require');
                 failed++;
-                authenticationService.login({
-                    'DeviceUUID': 'TEST_DEVICE_UUID',
-                    'DeviceToken': 'TEST_DEVICE_TOKEN',
-                    'DeviceOS': 'TEST_DEVICE_OS'
-                }, function (response, headers) {
-                    $http.defaults.headers.common['X-Device-Id'] = 'TEST_DEVICE_UUID';
-                    if (failed < 2)
-                        authService.loginConfirmed();
-                }, function (error) {
-                    console.log('login failed', error);
 
+                certService.then(function (response) {
+                    var publicKey = response.data;
+                    var now = new Date();
+                    var time = now.getTime();
+                    var loginData = angular.fromJson({
+                        'Now': parseInt(time / 1000),
+                        'DeviceUUID': 'TEST_DEVICE_UUID',
+                        'DeviceToken': 'TEST_DEVICE_TOKEN',
+                        'DeviceOS': 'TEST_DEVICE_OS'
+                    });
+                    console.log(JSON.stringify(loginData));
+
+                    var encrypt = new JSEncrypt();
+                    encrypt.setPublicKey(publicKey);
+                    var encrypted = encrypt.encrypt(JSON.stringify(loginData));
+
+                    authenticationService.login({
+                        'Login': encrypted
+                    }, function (response) {
+                        $http.defaults.headers.common['X-Device-Id'] = 'TEST_DEVICE_UUID';
+                        if (failed < 2)
+                            authService.loginConfirmed();
+                    }, function (error) {
+                        console.log('login failed', error);
+                    });
                 });
             });
             scope.$on('event:auth-loginConfirmed', function () {
