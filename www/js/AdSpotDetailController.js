6f51e8fc2abe7ac05439e0cdd7261468a6b2072b
redspy <redspy@lycos.co.kr>
2015-09-08 00:11:25 +0900

    댓글 기능 추가함 Fixed #23

--- www/js/AdSpotDetailController.js
+++ www/js/AdSpotDetailController.js
@@ -1,6 +1,6 @@
 angular.module('starter.controllers')
 
-    .controller('adspotdetailcontroller', ['$scope', '$stateParams', 'lightningDealService', '$q', 'IMAGE_ENDPOINT', '$timeout', function ($scope, $stateParams, lightningDealService, $q, IMAGE_ENDPOINT, $timeout) {
+    .controller('adspotdetailcontroller', ['$scope', '$stateParams', 'lightningDealService', '$q', 'IMAGE_ENDPOINT', '$timeout', 'lightningDealReplyService', function ($scope, $stateParams, lightningDealService, $q, IMAGE_ENDPOINT, $timeout, lightningDealReplyService) {
         $scope.name = "반짝 떨이";
         $scope.id = $stateParams.unitid;
         $scope.item = [];
@@ -20,26 +20,57 @@ angular.module('starter.controllers')
             return deferred.promise;
         }
 
-        getStoreInformation($scope.id, 1).then(function (data) {
-            console.log(data);
-            $scope.images = [];
-            $scope.item = data[0];
-            $scope.name = $scope.item.Name;
-            for (var i = 0; i < $scope.item.Images.length; i++) {
-                $scope.images.push($scope.getImageURL($scope.item.Images[i]));
-            }
-            var now = new Date();
-            var end = new Date($scope.item.EndDate);
+        $scope.refreshItems = function(){
+            getStoreInformation($scope.id, 1).then(function (data) {
+                console.log(data);
+                $scope.images = [];
+                $scope.item = data[0];
+                $scope.name = $scope.item.Name;
+                for (var i = 0; i < $scope.item.Images.length; i++) {
+                    $scope.images.push($scope.getImageURL($scope.item.Images[i]));
+                }
+                var now = new Date();
+                var end = new Date($scope.item.EndDate);
 
-            $scope.item.EndDate = Math.max((end.getTime() - (now.getTime() + now.getTimezoneOffset() * 60 * 1000)) / 1000, 0);
-        });
+                $scope.item.EndDate = Math.max((end.getTime() - (now.getTime() + now.getTimezoneOffset() * 60 * 1000)) / 1000, 0);
+            });
+
+        }
+
+        $scope.refreshItems();
 
         $scope.getImageURL = function (imageID) {
             return IMAGE_ENDPOINT + imageID;
         };
 
         $scope.doSomething = function () {
-            $scope.reply = "";
+            $scope.submitReply($scope.id);
+            $scope.reply = '';
+        };
+
+        $scope.submitReply = function (id) {
+            /*
+            if ('Id' in $scope.reply) {
+                lightningDealService.modify({
+                    id: $scope.reply.Id
+                }, $scope.reply, function () {
+                    $scope.refreshItems();
+                });
+            } else
+            */
+            var replyJSON = {
+                NickName : 'O',
+                Description : $scope.reply
+            }
+            {
+                lightningDealReplyService.create({
+                    id: id
+                }, replyJSON, function () {
+                    $scope.item = [];
+                    $scope.refreshItems();
+                });
+            }
+
         };
 
         $scope.favoriteClick = function () {
--- www/js/AdStoreDetailController.js
+++ www/js/AdStoreDetailController.js
@@ -20,24 +20,53 @@ angular.module('starter.controllers')
             return deferred.promise;
         }
 
-        getStoreInformation($scope.id, 1).then(function (data) {
-            console.log(data);
-            $scope.images = [];
-            $scope.item = data[0];
-            $scope.name = $scope.item.Name;
-            for (var i = 0; i < $scope.item.Images.length; i++) {
-                $scope.images.push($scope.getImageURL($scope.item.Images[i]));
-            }
-        });
+        $scope.refreshItems = function() {
+            getStoreInformation($scope.id, 1).then(function (data) {
+                console.log(data);
+                $scope.images = [];
+                $scope.item = data[0];
+                $scope.name = $scope.item.Name;
+                for (var i = 0; i < $scope.item.Images.length; i++) {
+                    $scope.images.push($scope.getImageURL($scope.item.Images[i]));
+                }
+            });
+        }
+
+        $scope.refreshItems();
 
         $scope.getImageURL = function (imageID) {
             return IMAGE_ENDPOINT + imageID;
         };
 
         $scope.doSomething = function () {
+            $scope.submitReply($scope.id);
             $scope.reply = "";
         };
 
+        $scope.submitReply = function (id) {
+            /*
+             if ('Id' in $scope.reply) {
+             lightningDealService.modify({
+             id: $scope.reply.Id
+             }, $scope.reply, function () {
+             $scope.refreshItems();
+             });
+             } else
+             */
+            var replyJSON = {
+                NickName : 'O',
+                Description : $scope.reply
+            };
+            {
+                introShopReplyService.create({
+                    id: id
+                }, replyJSON, function () {
+                    $scope.item = [];
+                    $scope.refreshItems();
+                });
+            }
+
+        };
         $scope.favoriteClick = function () {
             alert('Add to Favorite');
         };
@@ -56,21 +85,5 @@ angular.module('starter.controllers')
 
         refreshIntroShop();
 
-        $scope.submitReply = function (id) {
-            if ('Id' in $scope.reply) {
-                introShopReplyService.modify({
-                    id: $scope.reply.Id
-                }, $scope.reply, function () {
-                    refreshIntroShop();
-                });
-            } else {
-                introShopReplyService.create({
-                    id: id
-                }, $scope.reply, function () {
-                    refreshIntroShop();
-                });
-            }
-
-        };
 
     }]);
