a61fef7576dec9dcdf4dc86e181d4e47c271bcb5
redspy <redspy@lycos.co.kr>
2015-09-30 17:51:21 +0900

    서버주소 변경

--- www/js/config.js
+++ www/js/config.js
@@ -1,9 +1,9 @@
 angular.module('starter.controllers')
 
-    //.constant('SERVICE_ENDPOINT', 'http://222.122.143.163/undercity/api')
-    //.constant('IMAGE_ENDPOINT', 'http://222.122.143.163/undercity/api/images/')
-    .constant('SERVICE_ENDPOINT', 'http://192.168.1.2/undercity/api')
-    .constant('IMAGE_ENDPOINT', 'http://192.168.1.2/undercity/api/images/')
+    .constant('SERVICE_ENDPOINT', 'http://222.122.143.163/undercity/api')
+    .constant('IMAGE_ENDPOINT', 'http://222.122.143.163/undercity/api/images/')
+    //.constant('SERVICE_ENDPOINT', 'http://192.168.1.2/undercity/api')
+    //.constant('IMAGE_ENDPOINT', 'http://192.168.1.2/undercity/api/images/')
     .config(function($ionicConfigProvider) {
         $ionicConfigProvider.views.transition('ios');
         $ionicConfigProvider.tabs.style('standard').position('bottom');
